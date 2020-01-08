e958fbd8d0fc31ad797fdcec73fd8044b35bf367
eeshita19 <43987674+eeshita19@users.noreply.github.com>
2020-01-08 20:10:12 +0530

    Add files via upload

---
+++ assets/js/file.js
@@ -0,0 +1,45 @@
+ $("ul").on("click","li",function(){
+     $(this).toggleClass("completed");
+ });
+
+ $("ul").on("click","span" , function(event){
+     $(this).parent().fadeOut(500,function(){
+         $(this).remove();
+     });
+    event.stopPropagation();
+
+ });
+
+
+ $("input[type='text']").keypress(function(event){
+     if(event.which===13){
+         var todo= $(this).val();
+         $(this).val("");
+         //new li 
+         $("ul").append("<li><span><i class='fa fa-trash'></i></span> "+ todo + "</li>");
+     }
+ });
+
+ $(".fa-plus").click(function(){
+     $("input[type='text']").fadeToggle();
+ });
+
+
+ // $("li").click(function(){
+
+//     //if li is gray
+//     if ($(this).css("color")==="rgb(128, 128, 128)"){
+//         $(this).css({
+//             color: "black",
+//             textDecoration: "none"
+//         });
+
+//     }
+//     else{
+//     $(this).css({
+//         color: "gray",
+//         textDecoration: "line-through"
+//     });
+// }
+    
+//  });
